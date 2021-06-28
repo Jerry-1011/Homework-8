e30bd0391aa71c13fa3fce8b6572cfdb0665a4b2
Jerry <maozengs@gmail>
2021-06-28 10:07:01 -0400

    orgin

--- Homework/index.js
+++ Homework/index.js
@@ -1,1 +1,33 @@
-console.log(888);
+console.log("helloworld");
+const x = 10;
+const x = 25;
+let a = 10;
+let b = 25;
+a  = 10;
+b = 25;
+const str = 'helloworld';
+const Firstname = 'Jerry';
+const Lastname = 'suo';
+
+const age = 12;
+const price = 100.50;
+const point = -50;
+const IsJerryHandSome = true;
+const IsJerryMarried = false;
+
+
+const Shop = ['Cheese', 'apple', 'banana'];
+const Jerry = {
+    firstname = 'Jery',
+    lastname = 'suo',
+    age = 12,
+};
+
+let string1 = str;
+string1 = 'hello boy'
+console.log(string1);
+console.log(str);
+
+const all = Shop;
+console.log(Shop);
+console.log(all);
