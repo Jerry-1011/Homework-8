4c2d5a32bafa2b6cc65bf2d3ee33ec16fa9a5d1a
Jerry <maozengs@gmail>
2021-07-10 18:53:05 -0400

    orgin

---
+++ Homework/index3.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Q7 Translate border-left-width to borderLeftWidt hWrite the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
+//That is: removes all dashes, each word after dash becomes uppercased.
+
+const string = 'my-short-string';
+const arr = string.split('-');
+
+
+
+
+
+
+
+
+
+
+
+
+
